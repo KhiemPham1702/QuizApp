b32f8acb3dd8f39fc0515ca6fc0e134eaa8e29c1
P51 <skhiem17022002@gmail.com>
2022-04-28 13:34:43 +0700

    App project

---
+++ app/src/main/java/com/example/QuizApp_project/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.QuizApp_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.application.app.R;
+
+class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
